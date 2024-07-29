df3d8d3cc1a2c1ea039842a90280c1ea3372031d
reeshabh-k <161019958+reeshabh-k@users.noreply.github.com>
2024-07-29 22:05:39 +0530

    Update ultrasonic_balance.ino

--- SelfBalancing/selfBalancing_final/ultrasonic_balance.ino
+++ SelfBalancing/selfBalancing_final/ultrasonic_balance.ino
@@ -1,21 +1,65 @@
+#define leftMotorPWMPin   6
+#define leftMotorDirPin   7
+#define rightMotorPWMPin  5
+#define rightMotorDirPin  4
+
 const int trigR = 9;
 const int echoR = 10;
 const int trigL = 11;
 const int echoL = 12;
-// defines variables
+
+float KpTerm = 0;
+float KiTerm = 0;
+float KdTerm = 0;
+
+volatile float error=0;
+volatile float prevError=0;
+volatile float errorSum=0;
+
+const float targetAngle = 0;
+
+const float Kp  = 13;
+const float Kd = 0.5;
+const float Ki =  0;
+
 double distanceR;
 double distanceL;
 
+void setMotors(int leftMotorSpeed, int rightMotorSpeed) {
+  if(leftMotorSpeed >= 0) {
+    analogWrite(leftMotorPWMPin, leftMotorSpeed);
+    digitalWrite(leftMotorDirPin, LOW);
+  }
+  else {
+    analogWrite(leftMotorPWMPin, leftMotorSpeed*(-1));
+    digitalWrite(leftMotorDirPin, HIGH);
+  }
+  if(rightMotorSpeed >= 0) {
+    analogWrite(rightMotorPWMPin, rightMotorSpeed);
+    digitalWrite(rightMotorDirPin, LOW);
+  }
+  else {
+    analogWrite(rightMotorPWMPin, rightMotorSpeed*(-1));
+    digitalWrite(rightMotorDirPin, HIGH);
+  }
+}
+
 void setup() {
+  pinMode(leftMotorPWMPin, OUTPUT);
+  pinMode(leftMotorDirPin, OUTPUT);
+  pinMode(rightMotorPWMPin, OUTPUT);
+  pinMode(rightMotorDirPin, OUTPUT);
+
   pinMode(trigR, OUTPUT); // Sets the trigPin as an Output
   pinMode(echoR, INPUT); // Sets the echoPin as an Input
 
   pinMode(trigL, OUTPUT);
   pinMode(echoL, INPUT);
-  Serial.begin(9600); // Starts the serial communication
+
+  Serial.begin(115200);
 }
-void loop() {
-  // Clears the trigPin
+
+void loop () {
   digitalWrite(trigR, LOW);
   digitalWrite(trigL, LOW);
   delayMicroseconds(2);
@@ -33,4 +77,15 @@ void loop() {
   Serial.print(distanceR);
   Serial.print(" ");
   Serial.println(distanceL);
+
+  if (abs(distanceR-distanceL)>0.2) {
+    if (distanceR > distanceL) {
+      setMotors(-50, -50);
+    } else {
+      setMotors(50,50);
+    }
+    
+  } else {
+    setMotors(0,0);
+  }
 }
